af8755475a5ec8d52fea151382153c7ed188b447
Piyush <piyushmore109@gmail.com>
2024-03-03 01:51:32 +0530

    project setup

---
+++ backend/index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+
+
+
+
+
+app.get("/",(req,res)=>{
+    res.send("server running");
+});
+
+
+app.listen(3000,()=>{
+    console.log("App is running on port no: 3000");
+});
